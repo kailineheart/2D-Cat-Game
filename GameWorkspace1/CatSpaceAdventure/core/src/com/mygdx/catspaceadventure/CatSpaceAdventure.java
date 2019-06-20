e8c83baa520eecc715abd770e2fb0a295fdd0699
Kai Lineheart <kailineheart@yahoo.com>
2019-06-20 10:27:09 -0500

    Add files via upload

---
+++ GameWorkspace1/CatSpaceAdventure/core/src/com/mygdx/catspaceadventure/CatSpaceAdventure.java
@@ -0,0 +1,12 @@
+package com.mygdx.catspaceadventure;
+
+import com.badlogic.gdx.Game;
+
+public class CatSpaceAdventure extends Game {
+
+	
+	@Override
+	public void create () {
+	
+	}
+}
---
+++ GameWorkspace1/CatSpaceAdventure/core/src/com/mygdx/catspaceadventure/GameScreenManager.java
@@ -0,0 +1,56 @@
+package com.mygdx.catspaceadventure;
+
+import com.badlogic.gdx.Screen;
+
+public class GameScreenManager implements Screen{
+
+	private CatSpaceAdventure game;
+	
+	// public constructor method
+	public GameScreenManager(CatSpaceAdventure game){
+		this.game = game;
+	}
+	
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
